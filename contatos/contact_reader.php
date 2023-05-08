87b94360d6e9b05cad1db95ac20d01b38680bd83
Kaique Nobre <dankmemerson@tutanota.com>
2023-05-08 16:19:46 -0300

    Centralized the includes 	modified:   contatos/contact_reader.php 	modified:   contatos/index.php

--- contatos/contact_reader.php
+++ contatos/contact_reader.php
@@ -1,9 +1,11 @@
 <?php
-$read = unserialize(file_get_contents('Location: contatos/dbs/contatos.bin'));
+function echo_contacts()
+{
+    $read = unserialize(file_get_contents('Location: contatos/dbs/contatos.bin'));
 
-if ($read) {
-    foreach ($read as $contact) {
-        echo "<div class = 'box'>
+    if ($read) {
+        foreach ($read as $contact) {
+            echo "<div class = 'box'>
                     <h1>$contact[0]</h1>
                     <div class = 'formulary'>
                     <p class='mono'>Email</p><p class='mono'>$contact[1]</p>
@@ -15,7 +17,8 @@ if ($read) {
                     <p class='mono'>Whatsapp</p><p class='mono'>$contact[3]</p>
                     </div>
                 </div>";
+        }
+    } else {
+        echo "<p>Atualmente você não tem nenhum contato salvo nessa agenda, clique em \"novo contato\" para adicionar um contato.</p>";
     }
-} else {
-    echo "<p>Atualmente você não tem nenhum contato salvo nessa agenda, clique em \"novo contato\" para adicionar um contato.</p>";
 }
--- contatos/index.php
+++ contatos/index.php
@@ -4,9 +4,10 @@
 <head>
     <?php
     include("../_templates/echoer.php");
-    
+
+    include("contact_reader.php");
     include("contact_writer.php");
-    
+
     include("../_templates/head.php");
     head_constructor("Contatos");
     ?>
@@ -22,24 +23,24 @@
 
     <main>
         <div class="center_flex flex_column">
-            <form id="create_new" class="hidden box" method="post">
+            <form id="create_new" class="center_abs hidden box" method="post">
                 <h1>Novo contato</h1>
 
                 <div class="formulary">
                     <label for="nome">Nome</label>
-                    <input type="text" name="name" required>
+                    <input maxlength="64" type="text" name="name" required>
                 </div>
                 <div class="formulary">
                     <label for="email">Email</label>
-                    <input type="email" name="email" required>
+                    <input maxlength="64" type="email" name="email" required>
                 </div>
                 <div class="formulary">
                     <label for="tel"> N° telefone</label>
-                    <input type="tel" name="tel" required>
+                    <input maxlength="11" type="tel" name="tel" required>
                 </div>
                 <div class="formulary">
                     <label for="zap"> N° telefone (whatsapp)</label>
-                    <input type="tel" name="zap" required>
+                    <input maxlength="11" type="tel" name="zap" required>
                 </div>
 
                 <div>
@@ -47,17 +48,13 @@
                     <input type="submit" name="send" value="criar">
                 </div>
             </form>
-
-            <div class="contacts">
-                <?php
-                include("contact_reader.php");
-                ?>
-
+        </div>
+        <div class="contacts">
+                <?php echo_contacts(); ?>
                 <div>
                     <button onclick="toggle(document.getElementById('create_new'))" class="add_contact"></button>
                 </div>
             </div>
-        </div>
     </main>
 
     <?php
