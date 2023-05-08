1e8fb857d6664c64fbd12cd860246bf1e6d8f537
Kaique Nobre <dankmemerson@tutanota.com>
2023-05-08 13:58:01 -0300

    Anonymized some data 	modified:   home/index.php

--- home/index.php
+++ home/index.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <title>Inicio</title>
 
-    <?php include("../_templates/echoer.php");?>
+<head>
+    <?php
+    include("../_templates/echoer.php");
+    include("../_templates/head.php");
+    head_constructor("inicio");
+    ?>
 
 </head>
+
 <body>
-    <?php show("../_templates/header.html");?>
+    <?php show("../_templates/header.html"); ?>
     <main></main>
-    <?php show("../_templates/footer.html");?>
+    <?php show("../_templates/footer.html"); ?>
 </body>
+
 </html>
