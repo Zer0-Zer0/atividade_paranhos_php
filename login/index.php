b303b66dfba7e18eaa8563412388190783f00f2b
Kaique Nobre <dankmemerson@tutanota.com>
2023-05-08 14:44:51 -0300

    Centered the /login page 	modified:   login/index.php

--- login/index.php
+++ login/index.php
@@ -12,25 +12,27 @@
 <body>
     <?php show("../_templates/header.html"); ?>
     <main>
-        <form method="post" action="/login/code.php" class="box">
-            <h1>Seja bem-vindo!</h1>
+        <div class="center_abs">
+            <form method="post" action="/login/code.php" class="box">
+                <h1>Seja bem-vindo!</h1>
 
-            <div class="formulary">
-                <label for="username">
-                    Usuário
-                </label>
-                <input type="text" name="username">
-            </div>
-            <div class="formulary">
-                <label for="password">
-                    Senha
-                </label>
-                <input type="password" name="password">
-            </div>
-            <div class="formulary">
-                <input type="submit" name="send" value="Entrar">
-            </div>
-        </form>
+                <div class="formulary">
+                    <label for="username">
+                        Usuário
+                    </label>
+                    <input type="text" name="username">
+                </div>
+                <div class="formulary">
+                    <label for="password">
+                        Senha
+                    </label>
+                    <input type="password" name="password">
+                </div>
+                <div class="formulary">
+                    <input type="submit" name="send" value="Entrar">
+                </div>
+            </form>
+        </div>
     </main>
     <?php show("../_templates/footer.html"); ?>
 </body>
