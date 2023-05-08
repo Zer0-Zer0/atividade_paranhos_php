2342b8d114be1222fdf21d5c08590fa2f2e9c4bf
Kaique Nobre <dankmemerson@tutanota.com>
2023-05-08 14:39:42 -0300

    Made aestetical changes to the index.php in the /romano dir 	modified:   romano/index.php 	modified:   romano/style.css

--- romano/index.php
+++ romano/index.php
@@ -7,32 +7,36 @@
     include("../_templates/head.php");
     head_constructor("arabico para romano");
     ?>
+
     <script src="../_js/clamp.js"></script>
+
+    <link rel="stylesheet" href="/romano/style.css">
 </head>
 
 <body>
     <?php show("../_templates/header.html"); ?>
     <main>
-        <div class="center_abs box">
-            <h1>números arábicos para romanos</h1>
+        <div class="center_abs">
+            <div class="box">
+                <h1>números arábicos para romanos</h1>
 
-            <form method="post">
-                <div class="formulary">
-                    <p>1-3999</p>
-                </div>
-                <div class="formulary">
-                    <input onchange="clamp(this,1,3999)" type="number" name="decimal">
-                </div>
-                <div class="formulary">
-                    <input type="submit" name="send" value="Converter">
-                </div>
-            </form>
+                <form method="post" class="formulary">
+                        <label for="decimal">1-3999</label>
+                        <input onchange="clamp(this,1,3999)" type="number" name="decimal" required>
+                        <input type="submit" name="send" value="Converter">
+                </form>
+            </div>
 
-            <div id="out" class="formulary">
-                <p>Resultado: </p>
-                <?php
-                include("code.php");
-                ?>
+            <div class="box">
+                <div class="formulary">
+                    <p>Resultado: </p>
+
+                    <div id="out">
+                        <?php
+                        include("code.php");
+                        ?>
+                    </div>
+                </div>
             </div>
         </div>
 
