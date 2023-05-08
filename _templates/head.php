9ebced4645c65ebb894dd7b7eedb949bb1a3de4d
Kaique Nobre <dankmemerson@tutanota.com>
2023-05-08 13:53:01 -0300

    Re-referenced the style and the icon
    	modified:   _templates/head.php

--- _templates/head.php
+++ _templates/head.php
@@ -4,8 +4,8 @@ function head_constructor($title)
     echo ("
         <meta charset='UTF-8'>
     
-        <link rel='stylesheet' href='../css/style.css'>
-        <link rel='icon' href='../img/ico.webp'>
+        <link rel='stylesheet' href='../_css/style.css'>
+        <link rel='icon' href='../_img/ico.webp'>
     
         <title>$title</title>
         ");
