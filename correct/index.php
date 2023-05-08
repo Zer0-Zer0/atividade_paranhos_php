76ac2e488c5dd6628480a6515970d684667def25
Kaique Nobre <dankmemerson@tutanota.com>
2023-05-08 14:16:51 -0300

    Fixed the login page
    	new file:   correct/index.php
    	new file:   incorrect/index.php
    	deleted:    login/correct/index.html
    	deleted:    login/incorrect/index.html

---
+++ correct/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <?php
+        include("../_templates/echoer.php");
+        include("../_templates/head.php");
+        head_constructor("Acesso permitido");
+    ?>
+</head>
+
+<body>
+    <?php show("../_templates/header.html"); ?>
+    <main>
+        <h1 class="center_abs">ACESSO PERMITIDO - Bem-vindo de volta admin!</h1>
+    </main>
+    <?php show("../_templates/footer.html"); ?>
+</body>
+
+</html>
---
+++ incorrect/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <?php
+    include("../_templates/echoer.php");
+    include("../_templates/head.php");
+    head_constructor("Acesso negado");
+    ?>
+</head>
+
+<body>
+    <?php show("../_templates/header.html"); ?>
+    <main>
+        <h1 class="center_abs"> ACESSO NEGADO - Usuário ou senha incorretos</h1>
+    </main>
+    <?php show("../_templates/footer.html"); ?>
+</body>
+
+</html>
