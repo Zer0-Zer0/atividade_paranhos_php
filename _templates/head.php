7f4b7c467c9e751d3166cdc8e306f088b45bee56
Kaique Nobre <dankmemerson@tutanota.com>
2023-05-08 13:42:58 -0300

    Big overhaul 	new file:   _css/style.css 	new file:   _img/ico.webp 	new file:   _img/plus_icon.webp 	renamed:    cores/js/clamp.js -> _js/clamp.js 	new file:   _js/toggle.js 	new file:   _templates/echoer.php 	new file:   _templates/footer.html 	new file:   _templates/head.php 	new file:   _templates/header.html 	new file:   home/header.html 	new file:   home/index.php 	modified:   index.php

---
+++ _templates/head.php
@@ -0,0 +1,12 @@
+<?php
+function head_constructor($title)
+{
+    echo ("
+        <meta charset='UTF-8'>
+    
+        <link rel='stylesheet' href='../css/style.css'>
+        <link rel='icon' href='../img/ico.webp'>
+    
+        <title>$title</title>
+        ");
+}
---
+++ home/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+
+    <?php include("../templates/echoer.php");?>
+
+</head>
+<body>
+    <?php show("../templates/header.html");?>
+    <main></main>
+    <?php show("../templates/footer.html");?>
+</body>
+</html>
