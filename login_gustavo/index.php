13e6cb4771d924bd87f791785c55739a4511332c
Kaique Nobre <dankmemerson@tutanota.com>
2023-05-10 20:46:46 -0300

    fixed indentation 	modified:   login_gustavo/index.php

--- login_gustavo/index.php
+++ login_gustavo/index.php
@@ -2,69 +2,69 @@
 <html lang="pt-BR">
 
 <head>
-  <?php
-  include("../_templates/echoer.php");
+    <?php
+    include("../_templates/echoer.php");
 
-  include("../_templates/head.php");
-  head_constructor("Página de login");
-  ?>
+    include("../_templates/head.php");
+    head_constructor("Página de login");
+    ?>
 </head>
 
 <body>
-  <?php show("../_templates/header.html"); ?>
+    <?php show("../_templates/header.html"); ?>
 
-  <main>
-    <div class="flex-column">
-      <h1>Login</h1>
+    <main>
+        <div class="flex-column">
+            <h1>Login</h1>
 
-      <form method="post" class="box">
-        <h2>Por favor, faça o login</h2>
+            <form method="post" class="box">
+                <h2>Por favor, faça o login</h2>
 
-        <div class="flex-row spaced-between">
-          <label for="username">
-            Nome de usuário
-          </label>
+                <div class="flex-row spaced-between">
+                    <label for="username">
+                        Nome de usuário
+                    </label>
 
-          <input type="text" name="username" required>
+                    <input type="text" name="username" required>
+                </div>
+
+                <div class="flex-row spaced-between">
+                    <label for="password">
+                        Senha
+                    </label>
+
+                    <input type="password" name="password" required>
+                </div>
+
+                <div class="flex-row spaced-between">
+                    <input type="submit" value="Login">
+                </div>
+            </form>
         </div>
+    </main>
 
-        <div class="flex-row spaced-between">
-          <label for="password">
-            Senha
-          </label>
-
-          <input type="password" name="password" required>
-        </div>
-
-        <div class="flex-row spaced-between">
-          <input type="submit" value="Login">
-        </div>
-      </form>
-    </div>
-  </main>
-
-  <?php show("../_templates/footer.html"); ?>
+    <?php show("../_templates/footer.html"); ?>
 </body>
 
 </html>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Verifica se o usuário e a senha foram enviados pelo formulário
-  if (!empty($_POST["username"]) && !empty($_POST["password"])) {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    // Verifica se o usuário e a senha foram enviados pelo formulário
+    if (!empty($_POST["username"]) && !empty($_POST["password"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
 
-    // Verifica se o usuário e a senha estão corretos
-    if ($username === "123" && $password === "123") {
-      // Redireciona para a página de aprovação
-      header("Location: /login_gustavo/Aprovado.php");
-      exit();
-    } else {
-      // Redireciona para a página de reprovação
-      header("Location: /login_gustavo/Reprovado.php");
-      exit();
+        // Verifica se o usuário e a senha estão corretos
+        if ($username === "123" && $password === "123") {
+            // Redireciona para a página de aprovação
+            header("Location: /login_gustavo/Aprovado.php");
+            exit();
+        } else {
+            // Redireciona para a página de reprovação
+            header("Location: /login_gustavo/Reprovado.php");
+            exit();
+        }
     }
-  }
 }
 ?>
